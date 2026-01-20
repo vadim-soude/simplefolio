34b88f6d71e6c8c97ff242fb8497a5475192a182
Vadim Soudé <vadimsoude@gmail.com>
2026-01-20 19:54:06 +0100

    feat : override animation duration to 350ms

--- src/scripts/scrollReveal.js
+++ src/scripts/scrollReveal.js
@@ -4,6 +4,7 @@ export default function initScrollReveal(targetElements, defaultProps) {
   ScrollReveal({ reset: false });
 
   targetElements.forEach(({ element, animation }) => {
+    animation.duration = 350; // Override duration to 200ms for all animations
     ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
   });
 }
